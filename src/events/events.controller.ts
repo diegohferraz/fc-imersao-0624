eb3c8cb0a5cc5cd411e68913f7b881bd202b8c10
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-06-18 17:58:04 -0300

    feat: adds reserve spot feature

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { ReserveSpotRequest } from './request/reserve-spot.request';
 
 @Controller('events')
 export class EventsController {
@@ -30,5 +39,13 @@ export class EventsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.eventsService.remove(id);
+  }
+
+  @Post(':id/reserve')
+  reserveSpots(
+    @Body() reserveRequest: ReserveSpotRequest,
+    @Param('id') eventId: string,
+  ) {
+    return this.eventsService.reserveSpot({ ...reserveRequest, eventId });
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,22 +1,22 @@
 import { HttpCode, Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { ReserveSpotDto } from './dto/reserve-spot.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, SpotStatus, TicketStatus } from '@prisma/client';
 
 @Injectable()
 export class EventsService {
+  constructor(private prismaService: PrismaService) {}
 
-constructor(private prismaService: PrismaService) {
-
-}
-
-  create(createEventDto: CreateEventDto) { // Tem que tipar no DTO para ser possivel de usar as funções do prisma passando os valores direto
+  create(createEventDto: CreateEventDto) {
+    // Tem que tipar no DTO para ser possivel de usar as funções do prisma passando os valores direto
     return this.prismaService.event.create({
       data: {
         ...createEventDto,
-        date: new Date(createEventDto.date)
-      } // O Prisma ja sabe quais os campos deste objeto baseado na definição do schema
-    })
+        date: new Date(createEventDto.date),
+      }, // O Prisma ja sabe quais os campos deste objeto baseado na definição do schema
+    });
   }
 
   findAll() {
@@ -25,7 +25,7 @@ constructor(private prismaService: PrismaService) {
 
   findOne(id: string) {
     return this.prismaService.event.findUnique({
-      where: { id }
+      where: { id },
     });
   }
 
@@ -33,16 +33,96 @@ constructor(private prismaService: PrismaService) {
     return this.prismaService.event.update({
       data: {
         ...updateEventDto,
-        date: new Date(updateEventDto.date)
+        date: new Date(updateEventDto.date),
       },
-      where: { id }
+      where: { id },
     });
   }
 
   @HttpCode(204) // Muda o cód retornado na chamada HTTP
   remove(id: string) {
     return this.prismaService.event.delete({
-      where: { id }
+      where: { id },
     });
+  }
+
+  async reserveSpot(dto: ReserveSpotDto & { eventId: string }) {
+    //Primeiro eu vejo se os lugares existem
+    const spots = await this.prismaService.spot.findMany({
+      where: {
+        eventId: dto.eventId,
+        name: {
+          in: dto.spots, // Select * from spots where name in ('A1', 'A2')
+        },
+      },
+    });
+
+    // Se todos os tamanhos divergirem, quer dizer que não tenho todos os spots neste evento
+    if (spots.length !== dto.spots.length) {
+      const foundSpotsName = spots.map((spot) => spot.name);
+      const notFoundSpotsName = dto.spots.filter(
+        (spotName) => !foundSpotsName.includes(spotName),
+      );
+
+      throw new Error(`Spots ${notFoundSpotsName.join(', ')} not found`);
+    }
+
+    try {
+      // O modo natural do prisma é o autocommit, que feito uma transação ele salva no banco
+      // Nesse caso como as transações dependem uma das outras, eu tenho que criar isso dentro de uma transaction
+      // Isso garante que não tenha dados inconsistentes no banco caso uma das trasações falhar
+      const tickets = await this.prismaService.$transaction(
+        async (prisma) => {
+          await prisma.reservationHistory.createMany({
+            data: spots.map((spot) => ({
+              spotId: spot.id,
+              ticketKind: dto.ticket_kind,
+              email: dto.email,
+              status: TicketStatus.reserved,
+            })),
+          });
+
+          await prisma.spot.updateMany({
+            where: {
+              id: {
+                in: spots.map((spot) => spot.id),
+              },
+            },
+            data: {
+              status: SpotStatus.reserved,
+            },
+          });
+
+          const tickets = await Promise.all(
+            // USa o promis.all pq eu preciso do ID de cada um dos registros criados, se usar o createMAny ele nao retorna
+            spots.map((spot) =>
+              prisma.ticket.create({
+                data: {
+                  spotId: spot.id,
+                  ticketKind: dto.ticket_kind,
+                  email: dto.email,
+                },
+              }),
+            ),
+          );
+
+          return tickets;
+        },
+        { isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted },
+        // isolationLevel: Só vai conseguir ler registros que ja estejam commitados no banco, Só quero lidar com dados definitivos
+        // Preciso disso porque posso trabalhar com dados que outras transações ainda não fizeram commit
+      );
+
+      return tickets;
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        switch (e.code) {
+          case 'P2002': // unique constraint violation
+          case 'P2034': // transaction conflict
+            throw new Error('Some spots are already reserved');
+        }
+      }
+      throw e;
+    }
   }
 }
