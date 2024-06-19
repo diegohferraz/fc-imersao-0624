2954593fa922a90c9219c4c7f3a7ea4dc969e371
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-06-19 10:45:10 -0300

    refactor: refactors to use a lib core and app for simulate multiple partners

--- src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Isso serve para não precisar ficar importando a conexão do banco em cada um dos modulos
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService] // Tudo que eu crio dentro do modulo, é privado por padrão, se eu quero deixar publico, tenho que declarar no exports
-})
-export class PrismaModule {}
